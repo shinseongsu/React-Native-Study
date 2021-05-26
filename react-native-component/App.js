aac2d91914a6d312f2d62d04eafcd5f54b020028
Shin seongsu <tlstjdtn321@naver.com>
2021-05-26 23:17:28 +0900

    컴포넌트 사용해보기

---
+++ react-native-component/App.js
@@ -0,0 +1,60 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const name = 'Beomjun';
+  return (
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>
+    //     {/*  
+    //       삼항연산자
+
+    //       My name is {name === 'Beomjun' ? 'Beomjun Kim' : 'React Native'} 
+    //      */}
+
+
+    //     {/* 
+    //       if 문
+
+    //       {(() => {
+    //       if (name === 'Hanbit') return 'My name is Hanbit';
+    //       else if (name === 'Beomjun') return 'My name is Beojun';
+    //       else return 'My name is React Native';
+    //     })()} */}
+
+    //     { name === 'Beomjun' && (
+    //       <Text style={styles.text}>My name is Beomjun</Text>
+    //     )}
+    //     { name !== 'Beomjun' && (
+    //       <Text style={styles.text}>My name is Beomjun</Text>
+    //     )}
+    //   </Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
+    // 스타일링
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
