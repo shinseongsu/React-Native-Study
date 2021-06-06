13bfcef0c0dd4123bffa79dc0a6b6ef36409a485
Shin seongsu <tlstjdtn321@naver.com>
2021-06-06 23:46:14 +0900

    Alert 사용하기

---
+++ Typescript-project/Typescript-react-native/ch02_4/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {SafeAreaView, Alert, Button} from 'react-native'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Button title='Home'
+        onPress={() => Alert.alert('home.pressed.', 'message')} />
+    </SafeAreaView>
+  )
+}
